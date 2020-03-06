af06c133c438c65a99b927f4fd4bc5fcbe545540
Jacob Friedmann <jfriedmann@rover.com>
2020-03-06 08:47:18 -0800

    Back out incomplete projects

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,15 +5,18 @@ import WorkItem from '../components/WorkItem';
 import cueImage from '../images/cue.jpg';
 import readyImage from '../images/P3HeroFinal.jpg';
 import SHHImage from '../images/SHHhero2.jpg';
+import emeraldImage from '../images/emerald.jpg';
+import retrofitImage from '../images/P2HeroFinal.jpg';
 import GlobespinningImage from '../images/globespinning.jpg';
 import HazelImage from '../images/hazel.jpg';
+
 
 const Work = () => (
   <section className="container mb-5 pb-5" id="work">
     <div className="row">
       <h2 className="col-sm">Work</h2>
     </div>
-    <div className="row mb-4">
+    {/* <div className="row mb-4">
       <div className="col-md-4 mb-4 mb-md-0">
         <WorkItem
           link="/globespinning/"
@@ -30,7 +33,7 @@ const Work = () => (
           image={HazelImage}
         />
       </div>
-    </div>
+    </div> */}
     <div className="row mb-4">
       <div className="col-sm">
         <WorkItem
@@ -41,7 +44,7 @@ const Work = () => (
         />
       </div>
     </div>
-    <div className="row">
+    <div className="row mb-4">
       <div className="col-md-8 mb-4 mb-md-0">
         <WorkItem
           link="/ready/"
@@ -51,12 +54,32 @@ const Work = () => (
           cover={false}
         />
       </div>
+      <div className="col-md-4 mb-4 mb-md-0">
+        <WorkItem
+          link="/emerald/"
+          title="Emerald Seattle"
+          subtitle="Responsive Web App"
+          image={emeraldImage}
+          cover={false}
+        />
+      </div>
+    </div>
+    <div className="row">
       <div className="col-md-4">
         <WorkItem
           link="/seahappyhours/"
           title="SEA Happy Hours"
           subtitle="UX Design"
           image={SHHImage}
+          cover={false}
+        />
+      </div>
+      <div className="col-md-8">
+        <WorkItem
+          link="/retrofit/"
+          title="Retrofit Home"
+          subtitle="Information Architecture Improvements"
+          image={retrofitImage}
           cover={false}
         />
       </div>
