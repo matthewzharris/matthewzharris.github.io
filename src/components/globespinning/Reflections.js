70f79650ede0d58e4ba78af62381e431ba164910
Jacob Friedmann <jfriedmann@rover.com>
2020-05-05 08:43:46 -0700

    Add testemonials

--- src/components/globespinning/Reflections.js
+++ src/components/globespinning/Reflections.js
@@ -8,9 +8,26 @@ const Reflections = () => (
     <div className="row mb-4">
       <div className="col-sm">
         <h3>Next Steps</h3>
-        <p>The offshore dev team is currently building the redesigned app & website.</p>
+        <p>
+          The offshore dev team is currently building the redesigned app &
+          website.
+        </p>
       </div>
     </div>
+    <blockquote>
+      "Matt is an extremely talented UX Designer and we are very pleased with
+      his work. We hired Matt to redesign our travel app and website,
+      Globespinning, which was no small task. He listened to all of our needs
+      and immediately dove right in. Matt thoroughly researched our competition
+      while prioritizing our user’s requests and feedback to enhance their
+      experience. He always kept our company vision and goals top of mind every
+      step of the way. I appreciate Matt’s attention to detail, clear
+      communication with our Globespinning leadership team (both verbally and
+      visually), his openness to feedback, and never missing a deadline. Matt’s
+      creative talents and hard work have taken our app to the next level and we
+      are very happy that we hired him for the job." <br />
+      <br />- Amy Boes, CEO & Co-Founder of Globespinning
+    </blockquote>
   </Section>
 );
 
--- src/components/hazel/Reflections.js
+++ src/components/hazel/Reflections.js
@@ -15,6 +15,18 @@ const Reflections = () => (
         can help allow more design iteration
       </li>
     </ul>
+    <blockquote>
+      "Matt successfully delivered in a poised and professional manner. He
+      collaborated with us to draft the project plan and set incremental
+      milestones, and led all aspects of the design effort across initial
+      discovery, user interviews, visual and interaction design, and production
+      of final high fidelity mockups and style guide. Matt also brings excellent
+      soft skills to the table; he's an effective communicator with both
+      internal and external stakeholders. For the time that he was with us, he
+      really became part of our team and was a pleasure to work with. Matt can
+      be an asset to any team and organization." <br />
+      <br />- Arash Nasibi, CEO & Co-Founder of Hazel Analytics
+    </blockquote>
   </Section>
 );
 
