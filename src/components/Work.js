9e5324611a0d3be0f4ec8728dda97c74693b7dfe
Jacob Friedmann <jfriedmann@rover.com>
2020-05-18 08:31:49 -0700

    Clean up work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,8 +7,6 @@ import spendTrackerImage from '../images/SpendTrackerPortfolioImage.jpg';
 import designSystemImage from '../images/NEDesignSystemHero.jpg';
 import GlobespinningImage from '../images/globespinning.jpg';
 import HazelImage from '../images/hazel.jpg';
-import conceptImage from '../images/ConceptImagePortfolio.jpg';
-import creativeExplorerImage from '../images/CreativeExplorerMainImage.jpg';
 
 const Work = () => (
   <section className="container mb-5 pb-5" id="work">
@@ -36,15 +34,22 @@ const Work = () => (
     <div className="row mb-4">
       <div className="col-md-12">
         <WorkItem
+          link="/new-engen-design-system/"
+          title="New Engen Design System"
+          image={designSystemImage}
+        />
+      </div>
+    </div>
+    <div className="row mb-4">
+      <div className="col-md-8 mb-4 mb-md-0">
+        <WorkItem
           link="/spend-tracker/"
           title="Spend Tracker"
           subtitle="New product"
           image={spendTrackerImage}
         />
       </div>
-    </div>
-    <div className="row mb-4">
-      <div className="col-md-8 mb-4 mb-md-0">
+      <div className="col-md-4 mb-4 mb-md-0">
         <WorkItem
           link="/globespinning/"
           title="Globespinning"
@@ -52,34 +57,6 @@ const Work = () => (
           image={GlobespinningImage}
           cover={false}
           bgColor="black"
-        />
-      </div>
-      <div className="col-md-4 mb-4 mb-md-0">
-        <WorkItem
-          link="/creative-explorer/"
-          title="Creative Explorer"
-          subtitle="New product feature"
-          image={creativeExplorerImage}
-        />
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-md-6">
-        <h3>Design Systems</h3>
-        <WorkItem
-          link="/new-engen-design-system/"
-          title="New Engen Design System"
-          image={designSystemImage}
-        />
-      </div>
-      <div className="col-md-6">
-        <h3>Concepts</h3>
-        <WorkItem
-          cover={false}
-          link="/concepts/"
-          title="Concept Work"
-          image={conceptImage}
-          bgColor="white"
         />
       </div>
     </div>
